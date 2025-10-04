13a1ba0da43b351c3e983765ac3b6d1568dc564f
5at4am <satraj6465@gmail.com>
2025-10-05 00:07:48 +0530

    added comment

--- 4_loop/for_1.java
+++ 4_loop/for_1.java
@@ -1,3 +1,15 @@
+/*
+ * File: For1.java
+ * Description: This program demonstrates a for loop by printing a request for name input 10 times.
+ * Author: Satyam Raj
+ * Date: October 2025
+ * 
+ * Purpose: This program demonstrates how to:
+ * - Use a for loop with initialization, condition, and increment
+ * - Print a message multiple times using loop control
+ * - Properly close Scanner resources even if not directly used in this example
+ * - Understand the structure of for loops: for(initialization; condition; increment)
+ */
 import java.util.*;
 
 public class for_1 {
@@ -12,3 +24,5 @@ public class for_1 {
 
     }
 }
+// For loops: When you know exactly how many times to repeat something!
+// Fun fact: The three parts of a for loop (init, condition, increment) can be modified for different behaviors!
