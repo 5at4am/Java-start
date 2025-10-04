6d6d66d04333151f12dee0b89916aea4583b2252
5at4am <satraj6465@gmail.com>
2025-10-05 00:07:22 +0530

    added comment

--- 4_loop/num1_10.java
+++ 4_loop/num1_10.java
@@ -1,3 +1,16 @@
+/*
+ * File: Num1_10.java
+ * Description: This program prints numbers from 1 to 10 using a while loop.
+ * Author: Satyam Raj
+ * Date: October 2025
+ * 
+ * Purpose: This program demonstrates how to:
+ * - Initialize a counter variable
+ * - Use a while loop with a condition
+ * - Print numbers in sequence
+ * - Increment the counter to avoid infinite loops
+ * - Format output with spaces between numbers
+ */
 public class num1_10 {
     public static void main(String[] args) {
         int num = 1;
@@ -8,3 +21,5 @@ public class num1_10 {
         }
     }
 }
+// Simple loop counting: The foundation of many algorithms!
+// Fun fact: While loops are useful when you don't know in advance how many times you'll loop!
--- 4_loop/print_100.java
+++ 4_loop/print_100.java
@@ -1,3 +1,16 @@
+/*
+ * File: Print100.java
+ * Description: This program prints numbers from 0 to 100 followed by a greeting message using a while loop.
+ * Author: Satyam Raj
+ * Date: October 2025
+ * 
+ * Purpose: This program demonstrates how to:
+ * - Initialize a counter variable
+ * - Use a while loop to repeat actions
+ * - Print formatted output combining numbers and text
+ * - Increment the counter to avoid infinite loops
+ * - Show different ways to increment a variable (commented examples)
+ */
 public class print_100
 {
     public static void main(String args[])
@@ -11,3 +24,5 @@ public class print_100
         
     }
 }
+// Counting to 100 with a personal greeting - efficient or just fun?
+// Fun fact: Increment operations can be done in multiple ways: count++, count += 1, or count = count + 1!
