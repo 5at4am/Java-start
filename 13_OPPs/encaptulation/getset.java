2bab50ec910f448dba08128f2d41e00a44a1524a
5at4am <satraj6465@gmail.com>
2025-10-05 01:27:18 +0530

    added comment

--- 13_OPPs/encaptulation/getset.java
+++ 13_OPPs/encaptulation/getset.java
@@ -1,3 +1,24 @@
+/*
+ * File: Getset.java
+ * Description: This program demonstrates getter and setter methods for encapsulation in Java.
+ * Author: Satyam Raj
+ * Date: October 2025
+ * 
+ * Purpose: This program demonstrates how to:
+ * - Create getter and setter methods for class properties
+ * - Understand the concept of encapsulation in OOP
+ * - Control access to class fields through methods
+ * - Use access modifiers appropriately (public, private, etc.)
+ * 
+ * Key concepts:
+ * - Getter methods: Return the value of private fields (getcolor())
+ * - Setter methods: Set the value of private fields (settip())
+ * - Encapsulation: Controlling access to class data
+ * - this keyword: Refers to the current object instance
+ * 
+ * Note: The example has mixed access levels - color is public but accessed via getter,
+ * while tip is public but has a setter method. In proper encapsulation, fields should be private.
+ */
 package encaptulation;
 public class getset {
 
@@ -13,7 +34,7 @@ public class getset {
     }
 
 }
- 
+
 class pen {
     public String color;
 
@@ -29,3 +50,5 @@ class pen {
     }
 
 }
+// Getter and setter methods: Controlling access to object properties!
+// Fun fact: Proper encapsulation typically uses private fields with public getters/setters!
