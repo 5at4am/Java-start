c9fef185f41ec1228800323e856fa5c6b3e5b650
5at4am <satraj6465@gmail.com>
2025-10-06 23:37:53 +0530

    added comment

--- 10_2D_array/spiral.java
+++ 10_2D_array/spiral.java
@@ -58,7 +58,7 @@ public class spiral {
             ER--;
 
         }
-        //System.out.println();
+        System.out.println();
 
     }
 
--- 999_rev/spiral.java
+++ 999_rev/spiral.java
@@ -1,47 +1,43 @@
 import java.util.*;
 
 public class spiral {
-    public static void sp(int mat[][]){
+    public static void sp(int mat[][]) {
+
         int n = mat.length;
         int m = mat[0].length;
-        int sr = 0, er=n-1, sc=0, ec = m-1;
+        int sr =0, sc=0, er=n-1, ec=m-1;
 
-        while(sr<=er &&sc<=ec){
-            for(int j = sc; j<=ec;j++){
-                System.out.println(mat[sr][j]);
+        while (sr<=er && sc<=ec){
+            // top
+            for (int j = sc; j <=ec;j++){
+                System.out.print(mat[sr][j]+" ");
             }
+            // right
             for(int i = sr+1;i<=er;i++){
-                if(sc==ec){
-                    break;
-                }
-                System.out.print(mat[i][ec]);
-            }
-            for(int j = ec-1;j>=sc;j--){
-                System.out.print(mat[er][j]);
-
-            }
-            for(int i = er-1 ;i>=sr+1;i--){
                 if(sr==er){
                     break;
                 }
-                System.out.print(mat[i][sc]);
+                System.out.print(mat[i][ec]+" ");
+            }
+            // bottom
+            for(int j = ec-1; j>=sc;j--){
+                System.out.print(mat[er][j]+" ");
+            }
+            //left
+            for(int i = er-1;i>=sr+1;i--){
+                System.out.print(mat[i][sc]+" ");
             }
             sr++;
-            er--;
             sc++;
+            er--;
             ec--;
-
-
+            
         }
-
+        
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-       int mat[][]= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-         sp(mat);
-        sc.close();
-        // Your code ends here
+        int mat[][]= {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        sp(mat);
     }
 }
