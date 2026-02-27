df5baf9150acfd6098b5e01c2296a3d35cc7dc9a
5at4am <satraj6465@gmail.com>
2026-02-27 22:26:26 +0530

    add more lines of code

--- 16_LinkedList/zigzag.java
+++ 16_LinkedList/zigzag.java
@@ -1,4 +1,47 @@
 public class zigzag {
+
+    public static class Node {
+        Node next;
+        int data;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+
+    public Node mid(Node head) {
+
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+
+    }
+
+    public void addFirst(int data) {
+
+        // create new node
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // new node.next = head
+        newNode.next = head;
+
+        // head = new node
+        head = newNode;
+
+    }
+
     public static void main(String[] args) {
 
     }
