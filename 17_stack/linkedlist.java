ad2364f168dff60a9704616c61a364ded5eca678
5at4am <satraj6465@gmail.com>
2026-03-03 14:01:04 +0530

    add more lines of code

--- 17_stack/linkedlist.java
+++ 17_stack/linkedlist.java
@@ -1,16 +1,29 @@
-import java.util.ArrayList;
+public class linkedlist {
+    static class Node {
+        int data;
+        Node next;
 
-public class arraylist {
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
 
     static class Stack {
-        static ArrayList<Integer> list = new ArrayList<>();
+        static Node head = null;
 
         public static boolean isEmpty() {
-            return list.size() == 0;
+            return head == null;
         }
 
         public static void push(int data) {
-            list.add(data);
+            Node newNode = new Node(data);
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
 
         }
 
@@ -18,18 +31,19 @@ public class arraylist {
             if (isEmpty()) {
                 return -1;
             }
-            int top = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
+            int top = head.data;
+            head = head.next;
             return top;
+
         }
 
         public static int peek() {
             if (isEmpty()) {
                 return -1;
             }
-            return list.get(list.size() - 1);
-        }
+            return head.data;
 
+        }
     }
 
     public static void main(String[] args) {
@@ -43,8 +57,9 @@ public class arraylist {
         while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
-        }
-        // System.out.println(s.peek());
 
+        }
+        System.out.println(s.peek());
     }
+
 }
