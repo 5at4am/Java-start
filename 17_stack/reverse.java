5c0fe88eb242f1fd03b8da53e0ebb4c40a7a241b
5at4am <satraj6465@gmail.com>
2026-03-04 11:30:00 +0530

    add more lines of code

---
+++ 17_stack/reverse.java
@@ -0,0 +1,47 @@
+import java.util.*;;
+
+public class reverse {
+    public static void Bottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        Bottom(s, data);
+        s.push(top);
+    }
+
+    public static void reverseStack(Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        Bottom(s, top);
+
+    }
+
+    public static void print(Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(3);
+        s.push(2);
+        s.push(1);
+
+        // print Stack
+        // print(s);
+
+        // reverse stack
+        reverseStack(s);
+        // print
+        print(s);
+
+    }
+}
