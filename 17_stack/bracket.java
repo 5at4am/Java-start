7c2f46d6e5997476716c2d010013c2ed79c0e2c7
5at4am <satraj6465@gmail.com>
2026-03-06 16:51:51 +0530

    add more lines of code

--- 17_stack/bracket.java
+++ 17_stack/bracket.java
@@ -1,6 +1,11 @@
+import java.util.*;
+
 public class bracket {
+
     public static void main(String[] args) {
-        
+        Stack<Integer> s = new Stack<>();
+        s.push(2);
+
     }
-    
+
 }
