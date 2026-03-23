768027dc3dc89ba10be58a36606a7e558b780c29
5at4am <satraj6465@gmail.com>
2026-03-23 18:46:22 +0530

    add more lines of code

--- 18_queue/array.java
+++ 18_queue/array.java
@@ -57,9 +57,7 @@ public class array {
         q.add(1);
         q.add(2);
         q.add(3);
-        q.add(4);
-        q.add(5);
-        q.add(6);
+
 
         while (!q.isEmpty()) {
             System.out.println(q.peek());
