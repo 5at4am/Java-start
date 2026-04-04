26c2fd154186da23ee860805a4648b46648127dc
5at4am <satraj6465@gmail.com>
2026-04-04 11:49:31 +0530

    add more lines of code

--- 20_bitree/one.java
+++ 20_bitree/one.java
@@ -1,4 +1,51 @@
 import java.util.*;
+
 public class one {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    
+    static class BT {
+        static int idx = -1;
+
+        public static Node Build(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = Build(nodes);
+            newNode.right = Build(nodes);
+            return newNode;
+
+        }
+
+        public static void preorder(Node root) {
+            if (root == null) {
+                return;
+            }
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+    
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BT bt = new BT();
+        Node root = bt.Build(nodes);
+        System.out.println(root.data);
+
+        bt.preorder(root);
+    }
     
 }
