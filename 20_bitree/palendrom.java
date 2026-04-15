274eafe7f0567a4d947dbbaadbc0442f73b2aa2d
5at4am <satraj6465@gmail.com>
2026-04-15 21:17:45 +0530

    add more lines of code

--- 20_bitree/palendrom.java
+++ 20_bitree/palendrom.java
@@ -1,7 +1,11 @@
 public class palendrom {
+
+    public class int check(int arr[]){
+        
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,3,1};
-        n = arr.length;
+        int n = arr.length;
         for(int i = 0; i<n;i++){
             for(int j = n-1;j<0;j--){
                 if(arr[i]==arr[j]){
