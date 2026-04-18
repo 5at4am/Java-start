a3b0e2384b4a46c25b3f41648e8009f55d9ae53c
5at4am <satraj6465@gmail.com>
2026-04-18 21:31:37 +0530

    find kth level element

---
+++ 20_bitree/kth.java
@@ -0,0 +1,29 @@
+public class kth {
+    static class Node {
+        int data;
+        Node left;
+        Node Right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.Right = null;
+        }
+    }
+
+    static class BT {
+        static int idx = -1;
+
+        public static Node Build(int nodes[]) {
+            idx++;
+            if (nodes == null) {
+                
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello from kth!");
+    }
+}
