7ce8b3de6ca7fd5e155d1da7b12cd3edb1dc9b1a
5at4am <satraj6465@gmail.com>
2026-04-19 10:07:00 +0530

    kth level element

--- 20_bitree/kth.java
+++ 20_bitree/kth.java
@@ -1,13 +1,15 @@
+import org.w3c.dom.Node;
+
 public class kth {
     static class Node {
         int data;
         Node left;
-        Node Right;
+        Node right;
 
         public Node(int data) {
             this.data = data;
             this.left = null;
-            this.Right = null;
+            this.right = null;
         }
     }
 
@@ -16,14 +18,42 @@ public class kth {
 
         public static Node Build(int nodes[]) {
             idx++;
-            if (nodes == null) {
-                
-
+            if (nodes[idx] == -1) {
+                return null;
             }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = Build(nodes);
+            newNode.right = Build(nodes);
+            return newNode;
         }
     }
 
+    public static void klevel(Node root, int level, int k) {
+        if (root == null) {
+            return;
+        }
+        if (level == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+        klevel(root.left, level+1, k);
+        klevel(root.right, level+1, k);
+
+        
+    }
+
+
+
     public static void main(String[] args) {
-        System.out.println("Hello from kth!");
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        int k = 3;
+        klevel(root, 1, k);
     }
 }
