b2113af0a84b990184b04a95630de50162f853ee
5at4am <satraj6465@gmail.com>
2026-04-24 12:38:30 +0530

    search  added

---
+++ 21_bst/search.java
@@ -0,0 +1,14 @@
+public class search {
+    static class Node{
+        int data;
+        Node left;
+        Node right
+    }
+    public static void search(){
+
+    }
+
+
+    public static void main(String[] args) {
+    }
+}
