28ab7b70bca1cc367bff6f2317015aba6200a468
5at4am <satraj6465@gmail.com>
2026-04-28 21:06:06 +0530

    search node

--- 21_bst/search.java
+++ 21_bst/search.java
@@ -3,17 +3,23 @@ public class search {
         int data;
         Node left;
         Node right
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
     }
 
     public static void search(Node root, int k) {
         if(root == null) return null;
         if(root.data == k) return root;
 
-        
+
 
     }
 
 
     public static void main(String[] args) {
+
     }
 }
