88a3593f5ea46ff0011f169552ca604292ec1eb0
5at4am <satraj6465@gmail.com>
2026-05-17 19:40:28 +0530

    array revision remove duplicate

--- Practice/array/two_pointer.java
+++ Practice/array/two_pointer.java
@@ -16,10 +16,31 @@ public class two_pointer {
         return new int[] { -1, -1 };
     }
 
-    public static void main(String[] args) {
-        int[] arr = { 2, 7, 11, 15 };
-        int target = 9;
-        int[] result = twoSum(arr, target);
-        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    public static void remove_duplicate(int[] arr) {
+        
+        int s = arr[0];
+        int f = arr[1];
+        while (f < arr.length) {
+            if (arr[s] != arr[f]) {
+                s++;
+                arr[s] = arr[f];
+            }
+            f++;
+        }
     }
+
+    public static void main(String[] args) {
+
+        // int[] arr = { 2, 7, 11, 15 };
+        // int target = 9;
+        // int[] result = twoSum(arr, target);
+        // System.out.println("Indices: " + result[0] + ", " + result[1]);
+
+        // int[] arr = { 1, 2, 2, 3, 4, 4, 5 };
+        // remove_duplicate(arr);
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.print(arr[i] + " ");
+        // }
+
+    } 
 }
