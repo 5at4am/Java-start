ffd2378aca8585d23da4ff43a98683f00d24b3e0
5at4am <satraj6465@gmail.com>
2026-05-20 19:41:26 +0530

    sliding window

--- Practice/array/sliding_window.java
+++ Practice/array/sliding_window.java
@@ -1,15 +1,57 @@
 public class sliding_window {
+
+    public static void window(int[] arr, int k) {
+        int windowSum = 0, maxSum = 0;
+
+        for (int i = 0; i < k; i++)
+            windowSum += arr[i];
+        maxSum = windowSum;
+
+        for (int i = k; i < arr.length; i++) {
+            windowSum += arr[i] - arr[i - k]; // slide: add new, remove old
+            maxSum = Math.max(maxSum, windowSum);
+        }
+        System.out.println(maxSum);
+
+    }
+
+    public static void subarray(int[] arr) { // its time complexity is O(n^2)
+        int start = 0, end = 0;
+        while (start <= arr.length - 1) {
+            for (int i = start; i <= end; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            end++;
+            if (end == arr.length) {
+                start++;
+                end = start;
+            }
+        }
+
+    }
+
+    public static void sum(int[] arr, int k){
+        int start = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length-k; i++) {
+            int sum = 0;
+            for (int j = start; j < start+k; j++) {
+                sum = sum + arr[j];
+            }
+            max = Math.max(max, sum);
+            start++;
+        }
+        System.out.println(max);
+        
+    }
+
     public static void main(String[] args) {
-        int[] arr = {2, 4, 1, 7, 3};
-int k = 3, windowSum = 0, maxSum = 0;
 
-for (int i = 0; i < k; i++) windowSum += arr[i];
-maxSum = windowSum;
-
-for (int i = k; i < arr.length; i++) {
-    windowSum += arr[i] - arr[i - k];  // slide: add new, remove old
-    maxSum = Math.max(maxSum, windowSum);
-}
-System.out.println(maxSum);
+        int[] arr = { 2, 4, 1, 7, 3 };
+        int k = 3;
+        // window(arr, k);
+        // subarray(arr);
+        sum(arr, k);
     }
 }
