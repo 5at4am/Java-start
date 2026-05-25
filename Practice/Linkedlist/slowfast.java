460dbdcc9aac299ddedc2694baa2ec0fff88c971
5at4am <satraj6465@gmail.com>
2026-05-25 17:04:41 +0530

    two pointer, check cycle

--- Practice/Linkedlist/slowfast.java
+++ Practice/Linkedlist/slowfast.java
@@ -1,5 +1,41 @@
-public class slowfast {
-    public static void main(String[] args) {
-        System.out.println("Hello from slowfast!");
+    import java.util.*;
+
+    import org.w3c.dom.Node;
+
+    public class slowfast {
+
+        static class Node {
+            int data;
+            Node next;
+
+            Node(int data) {
+                this.data = data;
+                this.next = null;
+            }
+        }
+
+        public static boolean checkcycle(Node head) {
+            Node slow = head;
+            Node fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void main(String[] args) {
+            Node head = new Node(1);
+            head.next = new Node(2);
+            head.next.next = new Node(3);
+            head.next.next.next = new Node(4);
+
+            // Creating cycle
+            head.next.next.next.next = head;
+
+            System.out.println(checkcycle(head));
+        }
     }
-}
