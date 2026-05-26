69f079f3698dd10c6a1fd3f48cce93a5d248b10d
5at4am <satraj6465@gmail.com>
2026-05-26 16:17:27 +0530

    start cycle

--- Practice/Linkedlist/slowfast.java
+++ Practice/Linkedlist/slowfast.java
@@ -26,16 +26,35 @@
             }
             return false;
         }
+        public static void startCycle(Node head){
+            Node slow = head;
+            Node fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (slow == fast) {
+                    break;
+                }
+            }
+            fast = head;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            System.out.println(slow.data);
+
+        }
 
         public static void main(String[] args) {
             Node head = new Node(1);
             head.next = new Node(2);
             head.next.next = new Node(3);
             head.next.next.next = new Node(4);
-
+            head.next.next.next.next = new Node(3);
             // Creating cycle
             head.next.next.next.next = head;
 
-            System.out.println(checkcycle(head));
+            // System.out.println(checkcycle(head));
+            startCycle(head);
         }
     }
