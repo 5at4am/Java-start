27edc3c5c4bdff1d25681b3af61ae53a8f217d96
5at4am <satraj6465@gmail.com>
2026-06-14 09:12:21 +0530

    search in binary search tree

--- 21_bst/main.java
+++ 21_bst/main.java
@@ -1,10 +1,10 @@
 public class main {
-    static class Node{
+    static class Node {
         int data;
         Node left;
         Node right;
 
-        public Node(int data){
+        public Node(int data) {
             this.data = data;
             this.left = null;
             this.right = null;
@@ -13,23 +13,20 @@ public class main {
 
     // create a binary tree when the input is an array
 
-    public static Node insert(Node root, int val){
-        if(root == null){
+    public static Node insert(Node root, int val) {
+        if (root == null) {
             root = new Node(val);
             return root;
         }
-        if(root.data> val){
-            //left sub tree
+        if (root.data > val) {
+            // left sub tree
             root.left = insert(root.left, val);
-        }else{
-            //right sub tree
-            root.right = insert(root.right,val);
+        } else {
+            // right sub tree
+            root.right = insert(root.right, val);
         }
         return root;
     }
-
-
-
 
     // search on binary tree //? (BST)
 
@@ -57,25 +54,11 @@ public class main {
 
     }
 
-    public static boolean search(Node root, int key) {
-        if (root == null) {
-            return false;
-        }
-        if (root.data == key)
-            return true;
-        if (root.data > key) {
-            search(root.right, key);
-        } else {
-            search(root.left, key);
-        }
-        
-    }
+    public static void main(String[] args) {
 
-    public static void main(String[] args) {    
-
-        int value[]= {5,1,3,4,2,7};
-        Node root =null;
-        for(int i = 0;i<value.length;i++){
+        int value[] = { 5, 1, 3, 4, 2, 7 };
+        Node root = null;
+        for (int i = 0; i < value.length; i++) {
             root = insert(root, value[i]);
         }
         root = new Node(4);
@@ -86,7 +69,6 @@ public class main {
         root.right.right = new Node(6);
 
         System.out.println(search(root, 2));
-        
 
     }
 }
