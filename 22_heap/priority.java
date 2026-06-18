fc18bb3b1d84b69d933cadeff8ba0fe62c894b8f
5at4am <satraj6465@gmail.com>
2026-06-18 21:05:02 +0530

    student class queue

--- 22_heap/priority.java
+++ 22_heap/priority.java
@@ -1,15 +1,45 @@
 import java.util.*;
+
 public class priority {
-    public static void main(String[] args) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
-        pq.add(3);
-        pq.add(4);
-        pq.add(1);
-        pq.add(7);
-        while (!pq.isEmpty()) {
-            System.out.println(pq.peek());
-            pq.remove();
+
+    static class Student implements Comparable<Student> { // function overriding
+        String name;
+        int rank;
+
+        public Student(String name, int rank) {
+            this.name = name;
+            this.rank = rank;
         }
-    
+
+        @Override
+        public int compareTo(Student s2) {
+            return this.rank - s2.rank;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        // pq.add(3);
+        // pq.add(4);
+        // pq.add(1);
+        // pq.add(7);
+        // while (!pq.isEmpty()) {
+        // System.out.println(pq.peek());
+        // pq.remove();
+        // }
+
+        PriorityQueue<Student> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
+        pq.add(new Student("E", 5));
+        while (!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " -> " + pq.peek().rank);
+            pq.remove();
+
+        }
     }
 }
