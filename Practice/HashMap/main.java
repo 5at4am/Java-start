70cdc8b4809ec671ad8370cfb09a8a3f6a2fb595
5at4am <satraj6465@gmail.com>
2026-06-30 17:13:03 +0530

    ransome note

--- Practice/HashMap/main.java
+++ Practice/HashMap/main.java
@@ -2,20 +2,14 @@ import java.util.*;
 
 public class main {
     public static int firstunique(String s) {
-        if (s.length() == 0) {
-            return -1;
-        }
-        if (s.length() == 1) {
-            return 0;
-        }
+
         HashMap<Character, Integer> map = new HashMap<>();
         int n = s.length();
-        
-       
+
         for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
-            if (map.containsKey(ch + "")) {
-                map.put(ch,map.get(ch)+ 1);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) + 1);
             } else {
                 map.put(ch, 1);
             }
@@ -28,12 +22,34 @@ public class main {
         }
         return -1;
 
-        
+    }
+
+    public static boolean ransom(String ransom, String magazine) {
+        HashMap<Character, Integer> have = new HashMap<>();
+        HashMap<Character, Integer> need = new HashMap<>();
+
+        for (char c : ransom.toCharArray()) {
+            have.put(c, have.getOrDefault(c, 0) + 1);
+        }
+        for (char c : magazine.toCharArray()) {
+            need.put(c, need.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransom.toCharArray()) {
+            if (have.get(c) < need.get(c)) {
+                return false;
+            }
+        }
+        return true;
+
     }
 
     public static void main(String[] args) {
-        String s = "a";
-        System.out.println(firstunique(s));
+        // String s = "ab";
+        // System.out.println(firstunique(s));
+        String s = "aabc";
+        String t = "aabc";
+        System.out.println(ransom(s, t));
 
     }
 }
