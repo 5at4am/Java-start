2e6101c2316a7020b400240c87c0cd1623c8d93c
5at4am <satraj6465@gmail.com>
2026-06-30 15:19:59 +0530

    unique first key

--- Practice/HashMap/main.java
+++ Practice/HashMap/main.java
@@ -8,21 +8,21 @@ public class main {
         if (s.length() == 1) {
             return 0;
         }
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         int n = s.length();
-        char ch = s.charAt(0);
-        map.put(ch + "", 1);
-        for (int i = 1; i < n; i++) {
-            ch = s.charAt(i);
+        
+       
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
             if (map.containsKey(ch + "")) {
-                map.put(ch + "", map.get(ch + "") + 1);
+                map.put(ch,map.get(ch)+ 1);
             } else {
-                map.put(ch + "", 1);
+                map.put(ch, 1);
             }
         }
         for (int i = 1; i < n; i++) {
-            ch = s.charAt(i);
-            if (map.get(ch + "") == 1) {
+            char ch = s.charAt(i);
+            if (map.get(ch) == 1) {
                 return i;
             }
         }
