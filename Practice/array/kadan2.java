4c8c78b6eca3cba947bc33153373a56d747a71a8
5at4am <satraj6465@gmail.com>
2026-07-08 23:12:40 +0530

    kadan2_

--- Practice/array/kadan2.java
+++ Practice/array/kadan2.java
@@ -1,5 +1,7 @@
 public class kadan2 {
+    public static void kadans(int[] arr){
+        
+    }
     public static void main(String[] args) {
-        System.out.println("Hello from kadan2!");
     }
 }
